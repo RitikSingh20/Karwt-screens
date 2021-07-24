ed6cdb9de3f3d75e8741f5648fd0bb1d0fe0fcee
ritik723 <ritiksingh7239@gmail.com>
2021-07-24 11:28:04 +0530

    created Error 404 page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 
 import Policy from './components/Policy';
 import Trackorders from './components/Trackorders'
+import { Errorpage } from './components/Errorpage';
 
 
 function App() {
   return (
      <div>
-       <Policy/>
-       <Trackorders />
+       {/* <Policy/>
+       <Trackorders /> */}
+       <Errorpage/>
      </div>
   );
 }
